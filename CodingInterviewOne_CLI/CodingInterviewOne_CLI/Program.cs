119080b1fe4224803a42f5254597f67d28163dac
Bevan Coleman <817786+bevancoleman@users.noreply.github.com>
2023-03-27 08:30:26 +1030

    Base Hello World

--- CodingInterviewOne_CLI/CodingInterviewOne_CLI/Program.cs
+++ CodingInterviewOne_CLI/CodingInterviewOne_CLI/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using System.Runtime.InteropServices;
+using static ConsoleCli.Program;
+
+namespace ConsoleCli
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('n', "name", Required = true, HelpText = "Your name.")]
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // Parse command-line args and handle errors
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(RunOptions).WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> obj)
+        {
+            Console.WriteLine("Invalid Parameters.");
+            Environment.Exit(-1);
+        }
+
+        private static void RunOptions(Options options)
+        {
+            Console.WriteLine("Hello {0}", options.Name);
+            Environment.Exit(0);
+        }
+    }
+}
